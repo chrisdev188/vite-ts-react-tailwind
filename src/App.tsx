e4e10678c654285c0cdfc01f09b52a3dce48f762
Chris Bui <chrisbui.dev188@gmail.com>
2023-01-09 11:20:52 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-pink-500 text-center">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
